4fd01ce27c3a14ef56babfedd7e20bdc3f21d062
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2021-02-13 16:41:54 +0530

    feat: add create Item functionality

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get all Items.';
+  }
+
+  @Post()
+  create(@Body() { name, description, qty }: CreateItemDto): string {
+    return `Name: ${name}\nDescription: ${description}\nQuantity: ${qty}`;
+  }
+
+  @Put()
+  update(): string {
+    return 'Update Item';
+  }
+
+  @Delete()
+  delete(): string {
+    return 'Delete Item';
   }
 }
